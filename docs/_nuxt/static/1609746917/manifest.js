b425933371309df77a1225301a81b76443999b26
nekobato <nekobato@gmail.com>
2021-01-04 16:56:49 +0900

    generate docs with cname

--- docs/_nuxt/static/1609746917/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farticles","\u002Farticles\u002F2021-01-02-bought","\u002Farticles\u002F2020-12-28-init","\u002Farticles\u002F2021-01-03-vue-amazon-link"]})
